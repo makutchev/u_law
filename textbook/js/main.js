bde534647126904ce5ef518269ce47f1382fb18b
makutchev <114910420+makutchev@users.noreply.github.com>
2023-02-01 09:49:24 +0300

    Add files via upload

---
+++ textbook/js/main.js
@@ -0,0 +1,17 @@
+/*read more*/
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Читать далее";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Свернуть";
+    moreText.style.display = "inline";
+  }
+} 
+
